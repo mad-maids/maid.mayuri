1419eba35bd3d7c975ee241314d62690a36bcf14
Alexiy Rybin <alexiy.rybin@gmail.com>
2022-01-05 23:58:16 +0700

    New handler

--- src/utils/validator.ts
+++
@@ -1,9 +0,0 @@
-import { BotConfig } from "@utils/configs";
-import chalk from "chalk";
-
-export default async (config: BotConfig) => {
-  if (!config.token) {
-    console.log(chalk.red("[ERROR]") + " Token is required");
-    process.exit(1);
-  }
-};
